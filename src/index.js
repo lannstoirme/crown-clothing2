3d34cb9d944b31a79e2cfad74bad2b4131771ec8
Lori Woodward <loriwoodward@Loris-Air.lan>
2021-07-19 13:25:40 +1000

    Added directory into redux store

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store';
+import {store, persistor} from './redux/store';
+
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+    <PersistGate persistor={persistor}>
       <App />
+    </PersistGate>
     </BrowserRouter>
   </Provider>,
    document.getElementById('root')
