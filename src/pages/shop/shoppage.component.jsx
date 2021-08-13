80e8417d13549cf3ca3a9ee4e56a3811d268d212
Lori Woodward <loriwoodward@Loris-Air.lan>
2021-08-13 23:01:41 +1000

    amended shop page and added nested routing

--- src/pages/shop/shoppage.component.jsx
+++ src/pages/shop/shoppage.component.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionsOverView from '../../components/collections-overview/collections.overview.component';
+import CollectionPage from '../collection/category.component';
+
 
 const ShopPage = ({ match }) => {
-    console.log(match)
-    return(
+    console.log(match);
+    return (
     <div className='shop-page'>
         <Route exact path={`${match.path}`} component={CollectionsOverView} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
     </div>
-)};        
+    )};        
             
 export default ShopPage;
