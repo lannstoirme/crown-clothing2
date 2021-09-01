7c26b5f8334e4d8e45ee26026c61008c2927c226
Lori Woodward <loriwoodward@Loris-Air.lan>
2021-09-01 14:21:30 +1000

    added shop selectors

--- src/redux/shop/shop.selectors.jsx
+++ src/redux/shop/shop.selectors.jsx
@@ -1,24 +1,19 @@
-import { createSelector } from 'reselect'; 
-import memoize from 'lodash.memoize';
-
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
+import { createSelector } from 'reselect';
 
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
-    [selectShop],
-    shop => shop.collections
+  [selectShop],
+  shop => shop.collections
 );
 
-export const selectCollection = memoize((CollectionUrlParam) =>
-    createSelector(
-        [selectCollections],
-        (collections) => collection[collectionUrlParam]
-    )
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
 );
+
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+  );
